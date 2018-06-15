d4a74c77786aa6c5890325e210291091983f3f42
Ryan Lehmkuhl <rlehmkuhl@rlemkuhl.local>
2018-06-15 10:13:16 -0700

    Added up to totient function

--- 05-Ps_and_Qs/ps_and_qs.py
+++ 05-Ps_and_Qs/ps_and_qs.py
@@ -1,4 +1,5 @@
 import math
+import sys
 from cryptography.hazmat.primitives import serialization as serial
 from cryptography.hazmat.backends import default_backend
 
@@ -16,5 +17,16 @@ if __name__ == '__main__':
 
     n1 = pub1_rsa.public_numbers().n
     n2 = pub2_rsa.public_numbers().n
+    gcd = math.gcd(n1, n2)
+
+    for i in range(2, gcd/2+1):
+        if (gcd % i == 0):
+            print("GCD is not a prime")
+            sys.exit(1)
+    print("GCD is a prime, p1=p2=gcd. Continuing")
+
+    d1 = n1 / gcd
+    totient = (gcd - 1)(d1 - 1)
+    
 
 
