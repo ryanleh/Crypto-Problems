e35b7af9e846280ba66e23995ef24a5ec5baac7a
root <root@localhost.localdomain>
2018-06-15 09:00:33 -0700

    Finishing Bitcoin later after learning more about Elliptic curves

--- 04-Hello_Bitcoin/hello_bitcoin.py
+++ 04-Hello_Bitcoin/hello_bitcoin.py
@@ -8,8 +8,8 @@ PRIV_KEY = 'priv_key'
 if __name__ == '__main__':
     priv_key = int(open(PRIV_KEY, 'r').read())
     ec_priv_key = ec.derive_private_key(priv_key,
-                                        ec.EllipticCurve("secp256k1", 256),
+                                        ec.SECP256K1(),
                                         backend)
-    ec_pub_key = ec_priv_key.public_key(backend)
-
+    ec_pub_key = ec_priv_key.public_key()
+    print("NOT FINISHED")
 
