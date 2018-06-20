718ad24fae4ea6a5756cdf3fac7f6528917f5004
Ryan Lehmkuhl <ryanleh.ob@gmail.com>
2018-06-19 17:02:22 -0700

    Finished 06

--- 06-Affine_Cipher/affine_cipher.py
+++ 06-Affine_Cipher/affine_cipher.py
@@ -3,6 +3,7 @@
 
 from math import gcd
 from utils import modinv
+from hashlib import md5
 
 ALPHABET = 'alphabet.txt'
 CTEXT = 'ctext'
@@ -10,8 +11,9 @@ CTEXT = 'ctext'
 
 def affine_dec(ctext_num, a, b, m):
     a_inv = modinv(a, m)
-    ptext = [(a_inv*(num - b) % m) for num in ctext_num]
-    return ptext
+    ptext_num = [(a_inv * (num - b)) % m for num in ctext_num]
+    return ptext_num
+
 
 def is_word(word):
     """Simple word checking by asserting that a word doesn't have multiple
@@ -28,17 +30,20 @@ def is_word(word):
 
 
 if __name__ == '__main__':
-    alphabet = open(ALPHABET, 'r').read().strip()
-    ctext = open(CTEXT, 'r').read().strip()
+    alphabet = open(ALPHABET, 'r').read().replace('\n', '')
+    # Remove any leading or trailing whitespaces but newline
+    # might be a space
+    ctext = open(CTEXT, 'r').read().strip().replace('\n', ' ')
 
-    # Determine modulo and populate swap dictionaries
+    # Determine modulo and populate alphabet swap dictionaries
     m = len(alphabet)
     alpha_to_num = {}
     num_to_alpha = {}
     for i in range(m):
         alpha_to_num[alphabet[i]] = i
         num_to_alpha[i] = alphabet[i]
-
+    # Convert ciphertext to numbers
+    ctext_num = [alpha_to_num[c] for c in ctext]
 
     # Determine all possible a values
     pos_a = []
@@ -46,8 +51,6 @@ if __name__ == '__main__':
         if gcd(i, m) == 1:
             pos_a.append(i)
 
-    # Convert ciphertext to numbers
-    ctext_num = [alpha_to_num[c] for c in ctext]
 
     # For each a, brute force all possible b values, only print
     # decryptions which use valid punctuation
@@ -56,12 +59,7 @@ if __name__ == '__main__':
             ptext_num = affine_dec(ctext_num, a, b, m)
             ptext = "".join([num_to_alpha[num] for num in ptext_num])
             if all([is_word(word) for word in ptext.split(' ')]):
-                print("Possible decryption ({}, {}): ".format(a, b))
-                print(ptext + "\n\n")
-            
-
-
-
-
-
-
+                print("Possible decryption ({}, {}):".format(a, b))
+                print(ptext)
+                print("Hash: " + md5(ptext.encode('utf-8')).hexdigest())
+                print()
