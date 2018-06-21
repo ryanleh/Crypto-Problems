08dd97460639e41973b99b0e877c901daeb8f14c
Ryan Lehmkuhl <ryanleh.ob@gmail.com>
2018-06-21 08:43:46 -0700

    Initially solved rail_fence.  Going to add frequency analysis and clean up code next

--- 08-Rail_Fence/rail_fence.py
+++ 08-Rail_Fence/rail_fence.py
@@ -1,16 +1,20 @@
+from math import ceil
+
 CTEXT = "ctext"
 
 
 def rebuild_fence(ctext, rails, length):
     """Rebuilds the original fence format of the cipher"""
+    print(rails)
     fence = [[None] * length for n in range(rails)]
-    spacing = [(2 * rails - 2 - 2 * i) for i in range(rails // 2 + 1)] + [0]
+    spacing = [2 + 2*i for i in reversed(range(rails-1))] + [0]
     ctr, offset, rail = 0, 0, 0
     for c in ctext:
         if ctr + offset >= length:
             rail += 1
             offset += 1
             ctr = 0
+            spacing.sort(reverse=True)
         fence[rail][offset + ctr] = c
         if spacing[rail] == 0:
             spacing = spacing[::-1]
@@ -19,14 +23,10 @@ def rebuild_fence(ctext, rails, length):
     return fence
 
 
-
-
-
 if __name__ == '__main__':
-    ctext = open(CTEXT, 'r').read().replace(' ', '')
+    ctext = open(CTEXT, 'r').read().replace(' ', '').strip()
     length = len(ctext)
-    # TODO: Fix all the things
-    for rails in range(3, 10):
+    for rails in range(2, 20):
         fence = rebuild_fence(ctext, rails, length)
         ptext = [None] * length
         for rail in fence:
