6c9e79e60ecd6d071752f51dfcb4501d3d538a4c
Ryan Lehmkuhl <ryanleh.ob@gmail.com>
2018-06-22 20:09:29 -0700

    Add freq_weight and std_english frequencies

--- utils.py
+++ utils.py
@@ -15,7 +15,16 @@ def modinv(e, phi):
 def str_to_block(string, size):
         return [string[i:i+size] for i in range(0, len(string), size)]
 
-def freq_analysis(alphabet, ctext):
+
+std_english_freqs = {'e': 12.70, 't': 9.06, 'a': 8.17, 'o': 7.51, 'i': 6.97,
+                     'n': 6.75, 's': 6.33, 'h': 6.09, 'r': 5.99, 'd': 4.25,
+                     'l': 4.03, 'c': 2.78, 'u': 2.76, 'm': 2.41, 'w': 2.36,
+                     'f': 2.23, 'g': 2.02, 'y': 1.97, 'p': 1.93, 'b': 1.29,
+                     'v': 0.98, 'k': 0.77, 'j': 0.15, 'x': 0.15, 'q': 0.10,
+                     'z': 0.07}
+
+
+def freq_analysis(ctext, alphabet=std_english_freqs.keys()):
     """Returns a dictionary containing various letter frequencies.  Doesn't
     consider any characters not in the alphabet"""
     freqs = {}
@@ -25,7 +34,6 @@ def freq_analysis(alphabet, ctext):
     for char in ctext:
         if char not in alphabet:
             total -= 1
-            pass
         else:
             freqs[char] += 1
     if total == 0:
@@ -37,3 +45,11 @@ def freq_analysis(alphabet, ctext):
             freqs[char] = float(freqs[char]) / total * 100
         return freqs
 
+
+def freq_weight(obs_freqs, exp_freqs=std_english_freqs):
+    """Statistical weighing for observered character frequencies using
+    chi-squared testing https://en.wikipedia.org/wiki/Chi-squared_test"""
+    weight = 0
+    for char in obs_freqs:
+        weight += (exp_freqs[char] - obs_freqs[char]) ** 2 / exp_freqs[char]
+    return weight
